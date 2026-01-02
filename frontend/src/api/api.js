73cec4f4c3bbc45911f5181e6d5c095967bc6e1a
Sumit24C <sonkamblesumit236@gmail.com>
2026-01-02 22:05:20 +0530

    added seperate refreshApi

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -5,3 +5,8 @@ export const api = axios.create({
     baseURL: BASE_URL,
     withCredentials: true
 })
+
+export const refreshApi = axios.create({
+    baseURL: BASE_URL,
+    withCredentials: true
+});
--- frontend/src/hooks/useRefreshToken.js
+++ frontend/src/hooks/useRefreshToken.js
@@ -1,14 +1,11 @@
-import { api } from '@/api/api';
-import { useDispatch } from 'react-redux'
+import { refreshApi } from '@/api/api';
 
 const useRefreshToken = () => {
-    const dispatch = useDispatch();
     const refreshToken = async () => {
         try {
-            const response = await api.get('/user/refresh-token')
+            await refreshApi.get('/user/refresh-token')
             return true
         } catch (error) {
-            // console.error("refreshToken :: error :: ", error)
             return null
         }
     }
